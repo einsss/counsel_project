d3993cc77ddbf70d132a739f568fc92f64176d2f
Dong-jun Jang <einsss@gmail.com>
2016-01-24 10:26:17 +0900

    counsel project

---
+++ files/config/db.config.php
@@ -0,0 +1,38 @@
+<?php if(!defined("__XE__")) exit();
+$db_info = (object)array (
+  'master_db' => 
+  array (
+    'db_type' => 'mysql_innodb',
+    'db_port' => '3306',
+    'db_hostname' => '127.0.0.1',
+    'db_userid' => 'root',
+    'db_password' => 'wndud1023',
+    'db_database' => 'counsel',
+    'db_table_prefix' => 'xe_',
+  ),
+  'slave_db' => 
+  array (
+    0 => 
+    array (
+      'db_type' => 'mysql_innodb',
+      'db_port' => '3306',
+      'db_hostname' => '127.0.0.1',
+      'db_userid' => 'root',
+      'db_password' => 'wndud1023',
+      'db_database' => 'counsel',
+      'db_table_prefix' => 'xe_',
+    ),
+  ),
+  'default_url' => 'http://localhost:8080/',
+  'use_mobile_view' => 'Y',
+  'use_rewrite' => 'N',
+  'time_zone' => '-0800',
+  'use_prepared_statements' => 'Y',
+  'qmail_compatibility' => 'N',
+  'use_db_session' => 'N',
+  'use_ssl' => 'none',
+  'sitelock_whitelist' => 
+  array (
+    0 => '127.0.0.1',
+  ),
+);
